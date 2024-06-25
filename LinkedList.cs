0847670749da1c8f79f032e46bee15a5fd3dc5a1
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-06-25 20:36:15 +0300

    new class created to create a node

---
+++ LinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProject
+{
+    class LinkedList
+    {
+
+        private Node head;
+        private Node tail;
+        private int length; 
+        
+    
+        public LinkedList() 
+        {
+            head = null;
+            tail = null;
+        }
+
+        public LinkedList(int data) 
+        {
+            Node newNode = new Node(data);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+            Console.WriteLine(newNode.data);
+        }
+    }
+}
